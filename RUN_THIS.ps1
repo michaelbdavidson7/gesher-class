1b81d40f78457b4105d510415097711d6cbfded6
Michael Davidson <michaelbdavidson7@gmail.com>
2018-11-16 06:16:14 -0500

    Update RUN_THIS.ps1

--- RUN_THIS.ps1
+++ RUN_THIS.ps1
@@ -1,11 +1,14 @@
+# Assumes python, node/npm, and npm serverless -g are installed, and aws cli is installed and configured
+
 $GithubUrl = "https://github.com/michaelbdavidson7/gesher-class"
+$LoginUser = "Administrator"
 
 # Get the username
 $User = Read-Host -Prompt 'Input your name with no spaces'
 Write-Host "Hello '$User'"
 
 # Create the folders and download the code using the username
-iex "cd C:\Users\Administrator\Desktop"
+iex "cd C:\Users\$LoginUser\Desktop"
 iex "serverless create --template-url $GithubUrl --name $User --path $User"
 iex "cd $User"
 
